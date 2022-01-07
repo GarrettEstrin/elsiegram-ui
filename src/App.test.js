0f22ba714a51f7ebedce1b236903d5e1c8ed271e
Garrett Estrin <garrett.estrin@gmail.com>
2022-01-07 13:40:56 -0800

    Fix action

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
+import { testFunction } from '../src/Utilities/ExampleUtilities.js';
+
 test('app test', () => {
   expect('test').toBeTruthy();
+});
+
+test('example test', () => {
+  expect(testFunction()).toBeTruthy();
 });
